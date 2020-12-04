a363578d85cdd00d0a21d5c521911ff36a94a8bc
juanguren <juan_fe_7@live.com>
2020-12-04 01:56:05 -0500

    Valid result interface

---
+++ src/main/interfaces/entities.ts
@@ -0,0 +1,10 @@
+
+export interface validResult {
+    year: Number;
+    department: String;
+    name: String;
+    description: String;
+    sector: String;
+    product: String;
+    email: String;
+}

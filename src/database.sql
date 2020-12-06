71a40627059d63ddd5f12587c6afb103edc2b7ab
Juan Felipe <juan_fe_7@live.com>
2020-12-06 12:34:30 -0500

    Updated SQL

--- src/database.sql
+++ src/database.sql
@@ -4,5 +4,7 @@ CREATE TABLE users(
     id SERIAL PRIMARY KEY,
     age VARCHAR(3),
     email VARCHAR(20),
-    name VARCHAR(30)
+    name VARCHAR(30),
+    username VARCHAR(20)
+    isLogged VARCHAR(5) NOT NULL
 );

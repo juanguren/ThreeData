3efec121eac29762e8291c221ac396fbc2becbd6
Juan Felipe <juan_fe_7@live.com>
2020-12-06 14:03:48 -0500

    ENUM and new table

--- src/database.sql
+++ src/database.sql
@@ -8,3 +8,49 @@ CREATE TABLE users(
     username VARCHAR(20)
     isLogged VARCHAR(5) NOT NULL
 );
+
+CREATE TYPE department_list AS ENUM (
+    'AMAZONAS',
+    'ANTIOQUIA',
+    'ARAUCA',
+    'ARCHIPIELAGO DE SAN ANDRES',
+    'ATLÁNTICO',
+    'BOGOTÁ D.C.',
+    'BOLIVAR','BOYACÁ',
+    'CALDAS',
+    'CAQUETA',
+    'CASANARE',
+    'CAUCA',
+    'CESAR',
+    'CHOCO',
+    'CORDOBA',
+    'CUNDINAMARCA',
+    'GUAINIA',
+    'GUAVIARE',
+    'HUILA',
+    'LA GUAJIRA',
+    'MAGDALENA',
+    'META',
+    'NARIÑO',
+    'NORTE DE SANTANDER',
+    'PUTUMAYO',
+    'QUINDIO',
+    'RISARALDA',
+    'SANTANDER',
+    'SUCRE',
+    'TOLIMA',
+    'VALLE DEL CAUCA',
+    'VAUPES',
+    'VICHADA'
+)
+
+CREATE TABLE requested_data(
+    id SERIAL PRIMARY KEY,
+    year INT,
+    department department_list,
+    name VARCHAR(20),
+    description VARCHAR,
+    sector VARCHAR(25) ,
+    product VARCHAR(20),
+    email TEXT
+);

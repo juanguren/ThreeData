51c801dee361f9d1f8bc56f60443c47813e14edc
juanguren <juan_fe_7@live.com>
2020-12-26 17:06:04 -0500

    Message template

---
+++ src/main/view/messageTemplate.ts
@@ -0,0 +1,18 @@
+
+const messageString = 
+    `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>Document</title>
+    </head>
+    <body>
+        <h2>HEY!</h2>
+        <p>This a test message. I thus validate SendGrid's API.</p>
+    </body>
+    </html>`
+;
+
+export default messageString;

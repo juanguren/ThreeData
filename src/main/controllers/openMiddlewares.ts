2a7729535adeec649718801b61eb243358ff905d
juanguren <juan_fe_7@live.com>
2020-12-27 17:43:59 -0500

    No data model for this version

--- src/main/controllers/openMiddlewares.ts
+++ src/main/controllers/openMiddlewares.ts
@@ -1,9 +1,7 @@
 
 import { Request, Response, NextFunction } from 'express';
-import connectionPool from '../../pg_init';
 import axios, { AxiosResponse } from 'axios';
 import { validDataResult } from '../interfaces/entities';
-import dataModel from '../models/data.model';
 import sendGrid from '@sendgrid/mail';
 import {
     constructMessageLayout
@@ -51,41 +49,9 @@ const retrieveOpenData = async (
         });
         finalData == ""
          ? res.status(400).json({"message": "Empty response"})
-         : saveFoundData(req, res, mappedData);
+         : res.status(200).json(mappedData)
     } catch (error) {
         res.status(404).json(error);
-    }
-}
-
-const saveFoundData = async (
-    req : Request,
-    res: Response,
-    data: any
-) =>{
-    try {
-        data.map((fields: any) =>{
-            const { year, department, name, description, sector, product, email } = fields;
-            dataModel.saveDataResults({ year, department, name, description, sector, product, email })
-        });
-        res.status(201).json({message: "Row created succesfully"});
-    } catch (error) {
-        res.status(404).json({ message: "Error uploading information", error });
-    }
-}
-
-const retrieveSavedData  = async (
-    req : Request,
-    res: Response,
-    next: NextFunction
-) => {
-    try {
-        const dataResult = await dataModel.retrieveData();
-        if (dataResult.rows[0]) {
-            req.params.dataToSend = dataResult.rows[0];
-            next();
-        }
-    } catch (error) {
-        res.status(404).json({message: "No data was found", error});
     }
 }
 
@@ -131,8 +97,6 @@ const sendMessageWithData  = (
 
 export {
     retrieveOpenData,
-    saveFoundData,
     validateParams,
-    retrieveSavedData,
     sendMessageWithData
 }

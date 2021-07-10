2f7e08cf266fd3cc1948f4f7b9947ba6d7241dcf
Juan Felipe <juan_fe_7@live.com>
2021-07-10 16:38:16 -0500

    Linted file and removed experiments

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,16 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
 import moment from "moment";
-import userRouter from "./main/routes/users";
-import dataRouter from './main/routes/openData';
+import dataRouter from "./main/routes/openData";
 
 const PORT = 5000 || 3000;
 const app = express();
 
-app.get("/", (_req : Request, res : Response) =>{
-    res.status(200).json({msg: "HEY", date: moment(new Date())});
+app.get("/", (_req: Request, res: Response) => {
+  res.status(200).json({ msg: "HEY", date: moment(new Date()) });
 });
 
-app.use("/index", userRouter);
 app.use("/data", dataRouter);
 
 app.listen(PORT, () => {
-    console.log("Listening in port " + PORT);
-})
+  console.log("Listening in port " + PORT);
+});

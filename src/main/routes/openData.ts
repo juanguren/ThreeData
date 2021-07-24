c992b2cec28a54c94c35fdc857701457637b2651
Juan Felipe <juan_fe_7@live.com>
2021-07-23 21:06:23 -0500

    Improved data endpoint names

--- src/main/routes/openData.ts
+++ src/main/routes/openData.ts
@@ -3,12 +3,12 @@ import {
   retrieveOpenData,
   validateParams,
   sendMessageWithData,
-} from "../controllers/operations";
+} from "../controllers/data/data_operations";
 
 const dataRouter = Router();
 dataRouter.use(json());
 
-dataRouter.get("/data", validateParams, retrieveOpenData);
-dataRouter.post("/data", sendMessageWithData);
+dataRouter.get("/get", validateParams, retrieveOpenData);
+dataRouter.post("/send", sendMessageWithData);
 
 export default dataRouter;

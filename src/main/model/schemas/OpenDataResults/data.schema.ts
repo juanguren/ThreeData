494e3efbac7fac7e2745e4b578d4be75db5fffb0
Juan Felipe <juan_fe_7@live.com>
2021-08-01 12:33:09 -0500

    Correction to the DataSchema referencing User

--- src/main/model/schemas/OpenDataResults/data.schema.ts
+++ src/main/model/schemas/OpenDataResults/data.schema.ts
@@ -4,7 +4,7 @@ const DataSchema: Schema = new Schema({
   sector: { type: String, required: true },
   year: { type: String, required: true },
   timestamp: { type: Date, required: true },
-  queries: [{ type: Schema.Types.ObjectId, ref: "Data" }],
+  user: [{ type: Schema.Types.ObjectId, ref: "User" }],
 });
 
 export { DataSchema };

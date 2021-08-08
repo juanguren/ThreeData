84eaf899eb4120c0ce878333d77a534821240cb4
Juan Felipe <juan_fe_7@live.com>
2021-08-07 21:18:16 -0500

    Helper method arranging the dataResult in the specified data Schema format

--- src/main/controllers/utils.ts
+++ src/main/controllers/utils.ts
@@ -1,3 +1,5 @@
+import { IData } from "../model/schemas/OpenDataResults/data.type";
+
 const checkForValidEmail = (email: string) => {
   const emailRegex = new RegExp(
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
@@ -6,4 +8,17 @@ const checkForValidEmail = (email: string) => {
   return false;
 };
 
-export { checkForValidEmail };
+const organizeDataIntoRecords = (dataAPIResult: Array<any>) => {
+  const organizedData = dataAPIResult.map((key: any) => {
+    return {
+      sector: key.sector,
+      product: key.product,
+      name: key.name,
+      year: key.year,
+    };
+  });
+
+  return organizedData;
+};
+
+export { checkForValidEmail, organizeDataIntoRecords };

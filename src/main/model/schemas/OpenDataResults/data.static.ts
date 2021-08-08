7e9f71f91fc6b859a091616008f9377378b1be50
Juan Felipe <juan_fe_7@live.com>
2021-08-07 21:21:02 -0500

    Removed comment

--- src/main/model/schemas/OpenDataResults/data.static.ts
+++ src/main/model/schemas/OpenDataResults/data.static.ts
@@ -1,6 +1,5 @@
 import { IData } from "./data.type";
 import DataSchema from "./data.model";
-// * https://medium.com/@mendes.develop/joining-tables-in-mongodb-with-mongoose-489d72c84b60
 
 const saveDataRecords = async (dataObject: any): Promise<IData> => {
   try {

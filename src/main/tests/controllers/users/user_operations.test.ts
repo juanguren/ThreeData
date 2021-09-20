8d6d8c6e98f2ba974a0735adee701a0d5cfe03d0
Juan Felipe <juan_fe_7@live.com>
2021-09-19 20:58:41 -0500

    Implemented one method to test the creartion of a user (User class)

--- src/main/tests/controllers/users/user_operations.test.ts
+++ src/main/tests/controllers/users/user_operations.test.ts
@@ -4,10 +4,12 @@ import {
   deleteUser,
   getUser,
 } from '../../../controllers/users/user_operations';
-import { getUserResponse } from '../../mocks';
-import UserService from '../../../model/schemas/Users/users.static';
+import mocks from '../../mocks';
+import UserService, { User } from '../../../model/schemas/Users/users.static';
 import httpMock from 'node-mocks-http';
+//import { User } from '../../../model/schemas/Users/users.static';
 
+// This allows to spy on calls to the class constructor and all of its methods too!
 jest.mock('../../../model/schemas/Users/users.static');
 
 describe('Test GET service', () => {
@@ -26,12 +28,27 @@ describe('Test GET service', () => {
   });
   it('Should return the JSON user response', async () => {
     req.params.username = mockUsername;
-    (UserService.getUser as jest.Mock).mockReturnValue(getUserResponse);
+    (UserService.getUser as jest.Mock).mockReturnValue(mocks.getUserResponse);
     await getUser(req, res);
 
     expect(UserService.getUser).toReturn();
     expect(res._getJSONData()).toMatchObject({
       _id: '610f185071b55b27b738fa1d',
     });
+  });
+});
+
+describe('Tests Create New User', () => {
+  let req: any, res: any;
+  const mockUsername = 'juanguren';
+  beforeAll(async () => {
+    req = httpMock.createRequest();
+    res = httpMock.createResponse();
+  });
+  it('Should call the create user method inside the class', async () => {
+    req.body = mocks.createUserBody;
+    await createNewUser(req, res);
+
+    expect(User).toHaveBeenCalledTimes(1);
   });
 });

38d0ae5fe2056790d90875f5ac068ab212b7fc8b
Juan Felipe <juan_fe_7@live.com>
2021-10-18 17:26:09 -0500

    Linted and included a catcher for the database connection method

--- src/main/model/config.ts
+++ src/main/model/config.ts
@@ -1,27 +1,30 @@
-import Mongoose from "mongoose";
-import dotenv from "dotenv";
+import Mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-const databaseConnection = async () => {
+const databaseConnection = () => {
   const { DB_PASSWORD } = process.env;
   const URI = `mongodb+srv://juanguren:${DB_PASSWORD}@cluster0.3u8p3.mongodb.net/three_data?retryWrites=true&w=majority`;
+  try {
+    Mongoose.connect(URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
 
-  Mongoose.connect(URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  });
-
-  // Helpers
-  const database = Mongoose.connection;
-  database.once("open", async () => {
-    console.log("Connected to database");
-  });
-  database.on("error", () => {
-    console.log("Error connecting to database");
-  });
+    // Helpers
+    const database = Mongoose.connection;
+    database.once('open', async () => {
+      console.log('Connected to database');
+    });
+    database.on('error', () => {
+      console.log('Error connecting to database');
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default databaseConnection;
